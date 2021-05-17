79149bf5cffcf959eea739b7b4a7d2975ecc8da9
dongdayang <76395228+dongdayang@users.noreply.github.com>
2021-05-17 10:34:25 +0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -2,13 +2,13 @@ import streamlit as st
 import pandas as pd
 
 
-def plot(excel_file):
-    st.title('招聘数据')
+def plot(excel_file,sheet_selected):
+    st.title('招聘数据分析')
     state_selected = st.selectbox(
         '选择数据',
         ['组织区域', '部门\校区'],
     )
     if excel_file is not None:
         df = excel_file[state_selected].value_counts()
-        st.write(state_selected + "招聘直方图")
+        st.write(sheet_selected+state_selected + "招聘直方图")
         st.bar_chart(df)
