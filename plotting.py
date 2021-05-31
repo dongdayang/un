f3648fb487a059a0de016a78f62d984d09daa9cb
dongdayang <76395228+dongdayang@users.noreply.github.com>
2021-05-31 14:24:12 +0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -7,7 +7,7 @@ fe = fm.FontEntry(
     name='Microsoft YaHei')
 fm.fontManager.ttflist.insert(0, fe) # or append is fine
 
-plt.rcParams['font.family'] = ['Microsoft YaHei']  # 用来正常显示中文标签
+plt.rcParams['font.family'] = fe.name # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 state_selected = None
