9149e7ab0f97dbcf00afe57a11dc774692c232c9
dongdayang <76395228+dongdayang@users.noreply.github.com>
2021-05-31 14:21:02 +0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import matplotlib.font_manager as font_manager
 
-font_dirs = ['/', ]
+font_dirs = ['./', ]
 font_files = font_manager.findSystemFonts(fontpaths=font_dirs)
 font_list = font_manager.createFontList(font_files)
 font_manager.fontManager.ttflist.extend(font_list)
