4d9f103ce08505ad8a4a974cf81e3297e3e2100e
dongdayang <76395228+dongdayang@users.noreply.github.com>
2021-05-31 13:43:31 +0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,21 +1,37 @@
+import matplotlib.pyplot as plt
 import streamlit as st
-import pandas as pd
+
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 state_selected = None
 
+st.set_option('deprecation.showPyplotGlobalUse', False)
+
 
 def plot(excel_file, excel_file2, excel_file3, sheet_selected):
-    st.title('招聘数据分析')
-    state_selected = st.selectbox(
+    state = st.selectbox(
         '选择数据',
         ['组织区域', '部门\校区'],
     )
-    if state_selected is not None:
-        df = excel_file[state_selected].value_counts()
-        st.write(sheet_selected + state_selected + "招聘直方图")
+    if state is not None:
+        df1 = excel_file2[state].value_counts()
+        df2 = excel_file3[state].value_counts()
+        x = df1.index
+        x1 = df2.index
+        y = df1
+        y1 = df2
+
+        plt.bar(x, y, align='center')
+        plt.bar(x1, y1, color='orange', align='center')
+        plt.legend(labels=['入职', '离职'])  # 图例
+        plt.title('入职离职招聘直方图')
+        plt.ylabel('入离职数据')
+        plt.xlabel(state)
+        plt.xticks(rotation=90)
+
+        st.pyplot()
+
+        df = excel_file[state].value_counts()
+        st.write(sheet_selected + state + "招聘直方图")
         st.bar_chart(df)
-        df1 = excel_file2[state_selected].value_counts()
-        df2 = -excel_file3[state_selected].value_counts()
-        df3 = pd.concat([df1, df2])
-        st.write("入职离职招聘直方图")
-        st.bar_chart(df3)
