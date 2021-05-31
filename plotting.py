bb75c7bc7fa15709acc95b5da8e5e444b1cd5734
dongdayang <76395228+dongdayang@users.noreply.github.com>
2021-05-31 13:52:17 +0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
 import streamlit as st
 
-#plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']  # 用来正常显示中文标签
-#plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
+plt.rcParams['font.sans-serif'] = ['Microsoft YaHei']  # 用来正常显示中文标签
+plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 state_selected = None
 
