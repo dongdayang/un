6ca0548a46aaa4c8604fbe090ab350e5c3b60b6a
dongdayang <76395228+dongdayang@users.noreply.github.com>
2021-06-02 12:50:57 +0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -2,8 +2,8 @@ import matplotlib.pyplot as plt
 import streamlit as st
 import matplotlib.font_manager as fm
 
-fm.FontProperties(fname='./Microsoft-YaHei.tff')
-plt.rcParams['font.family'] = ['Microsoft-YaHei']  # 用来正常显示中文标签
+myfont = fm.FontProperties(fname='./Microsoft-YaHei.tff')
+#plt.rcParams['font.family'] = ['Microsoft-YaHei']  # 用来正常显示中文标签
 plt.rcParams['axes.unicode_minus'] = False  # 用来正常显示负号
 
 state_selected = None
@@ -26,9 +26,9 @@ def plot(excel_file, excel_file2, excel_file3, sheet_selected):
 
         plt.bar(x, y, align='center')
         plt.bar(x1, y1, color='orange', align='center')
-        plt.legend(labels=['入职', '离职'])  # 图例
-        plt.title('入职离职招聘直方图')
-        plt.ylabel('入离职数据')
+        plt.legend(labels=['入职', '离职'],fontproperties=myfont)  # 图例
+        plt.title('入职离职招聘直方图',fontproperties=myfont)
+        plt.ylabel('入离职数据',fontproperties=myfont)
         plt.xlabel(state)
         plt.xticks(rotation=90)
 
