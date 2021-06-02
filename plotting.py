94397430cba4acb4eb2622af7229b38eb2ff6fae
dongdayang <76395228+dongdayang@users.noreply.github.com>
2021-06-02 12:57:00 +0800

    Update plotting.py

--- plotting.py
+++ plotting.py
@@ -31,8 +31,8 @@ def plot(excel_file, excel_file2, excel_file3, sheet_selected):
         plt.bar(x, y, align='center')
         plt.bar(x1, y1, color='orange', align='center')
         plt.legend(labels=['入职', '离职'])  # 图例
-        plt.title('入职离职招聘直方图',fontproperties=myfont)
-        plt.ylabel('入离职数据',fontproperties=myfont)
+        plt.title('入职离职招聘直方图')
+        plt.ylabel('入离职数据')
         plt.xlabel(state)
         plt.xticks(rotation=90)
 
